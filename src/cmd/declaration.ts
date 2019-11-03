4714d9f72aae5ff8968e49b4e1aa254b823786f6
mmktomato <mkt.motohashi@gmail.com>
2019-11-03 23:43:22 +0900

    emit types

--- src/cmd/declaration.ts
+++ src/cmd/declaration.ts
@@ -1,14 +1,34 @@
 export const createInterface = (id: string, values: string[]) => {
+  // TODO: Looks too ugly!. Separate formatting logic.
+
   if (values.length < 1) {
     return `interface ${id} {
-  id: ${id};
+  id: "${id}";
 }`;
   }
 
   return `interface ${id} {
-  id: ${id};
+  id: "${id}";
   values: {
     ${values.map(value => `${value}: string;`).join("\n    ")}
   };
+}`;
+};
+
+export const createMessageTokenUnion = (interfaceNames: string[]) => {
+  // TODO: Add new line because this will be a very long line.
+  return `type MessageTokenType = ${interfaceNames.join(" | ")};`;
+};
+
+export const createExportedTranslateFunction = () => {
+  return "export const translate: (token: MessageTokenType) => string;";
+};
+
+export const wrapWithDeclare = (interfaces: string[], messageTokenUnion: string, translateFunction: string) => {
+  // TODO: Format
+  return `declare module 'wores' {
+  ${interfaces.join("\n")}
+  ${messageTokenUnion}
+  ${translateFunction}
 }`;
 };
--- src/cmd/wores.ts
+++ src/cmd/wores.ts
@@ -4,9 +4,15 @@ import { parse, TYPE } from "intl-messageformat-parser";
 import { resolve } from "path";
 import { exit, argv } from "process";
 
-import { createInterface } from "./declaration";
+import {
+  createInterface,
+  createMessageTokenUnion,
+  createExportedTranslateFunction,
+  wrapWithDeclare,
+} from "./declaration";
 import { isFlatStringObject } from "./util";
 
+// TODO: Fix this.
 const path = argv.length < 3 ? "example/en.json" : argv[2];
 const absPath = resolve(path);
 
@@ -16,17 +22,22 @@ import(absPath)
       throw new TypeError(`"${absPath}" must be flat and string-valued JSON.`);
     }
 
-    const entries = Object.entries(obj);
+    const interfaceNames = Object.keys(obj);
+    // TODO: Validate interfaceNames.
 
-    entries.forEach(([key, value]) => {
-      const ast = parse(value);
+    const interfaces = interfaceNames.map(interfaceName => {
+      const ast = parse(obj[interfaceName]);
       const vars = ast
         .filter(node => node.type !== TYPE.literal)
         .map(node => node.value);
 
-      const interfaceText = createInterface(key, vars);
-      console.log(interfaceText);
+      return createInterface(interfaceName, vars);
     });
+
+    const messageTokenUnion = createMessageTokenUnion(interfaceNames);
+    const translateFunction = createExportedTranslateFunction();
+
+    console.log(wrapWithDeclare(interfaces, messageTokenUnion, translateFunction));
   })
   .catch(err => {
     if (err instanceof Error) {
