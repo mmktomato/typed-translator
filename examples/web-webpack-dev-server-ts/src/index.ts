7686e4255633143c1959d3eec6ec50f1735099cd
mmktomato <mkt.motohashi@gmail.com>
2020-03-12 00:58:55 +0900

    Add module example (#5)

---
+++ examples/web-webpack-dev-server-ts/src/index.ts
@@ -0,0 +1,41 @@
+import { initTranslation, setLocale, translate } from "typed-translator";
+
+import en from "../resources/en";
+import ja from "../resources/ja";
+
+// Set messages.
+const dictonaries = { en, ja };
+initTranslation(dictonaries);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const radios: NodeListOf<HTMLInputElement> = document.querySelectorAll("input[name='lang']");
+
+  radios.forEach(radio => {
+    radio.addEventListener("change", () => {
+      onLocaleChange(radio.value);
+    });
+  });
+});
+
+const onLocaleChange = (locale: string) => {
+  // Set locale
+  setLocale(locale);
+  document.querySelector("#key1")!.innerHTML = translate({ id: "key1" });
+  document.querySelector("#key2")!.innerHTML = translate({ id: "key2", values: { name: "Alice" } });
+  document.querySelector("#key3")!.innerHTML = translate({ id: "key3", values: { name: "Bob", age: "20" } });
+  document.querySelector("#key4")!.innerHTML = translate({ id: "key4", values: { age: "25" } });
+  document.querySelector("#key5")!.innerHTML = translate({ id: "key5" });
+  document.querySelector("#key6")!.innerHTML = translate({ id: "key6" });
+
+  // The following line isn't allowed by TypeScript compiler.
+  // Because "key0" doesn't exist.
+  // translate({ id: "key0" });
+
+  // The following line isn't allowed by TypeScript compiler.
+  // Because "key1" doesn't have any variables.
+  // translate({ id: "key1", values: { test: "test" } });
+
+  // The following line isn't allowed by TypeScript compiler.
+  // Because "key2" doesn't have the variable named "myName".
+  // translate({ id: "key2", values: { myName: "Alice" } });
+};
